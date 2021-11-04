7758b68c9f8920013bc96e5067b01bcc179d8713
kk <kkjiweb@gmail.com>
2021-11-04 17:17:57 +0530

    chlo

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request , 'about.html')
+
+def contact(request):
+    return HttpResponse("Contact Page")
